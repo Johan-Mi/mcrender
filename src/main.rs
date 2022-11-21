f76cb8815f90a5c56180a385b7bb6e4ba023f87f
Johan-Mi <johanmi@protonmail.com>
2022-11-21 13:14:16 +0100

    Read area as a command line argument

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,23 @@ pub struct Options {
 }
 
 fn main() {
-    let world_path = std::env::args()
-        .nth(1)
-        .unwrap_or_else(|| "world".to_owned());
+    let mut args = std::env::args().skip(1);
+    let world_path = args.next().unwrap_or_else(|| "world".to_owned());
     let resource_pack_path = PathBuf::from(
-        std::env::args()
-            .nth(2)
-            .unwrap_or_else(|| "resource-pack".to_owned()),
+        args.next().unwrap_or_else(|| "resource-pack".to_owned()),
     );
+    let area = match args.next() {
+        None => IVec2::new(0, 0)..IVec2::new(32, 16),
+        Some(s) => {
+            let [x1, z1, x2, z2]: [i32; 4] = s
+                .split(',')
+                .map(|coord| coord.parse().unwrap())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            IVec2::new(x1, z1)..IVec2::new(x2, z2)
+        }
+    };
 
     let options = Options {
         resource_pack_path,
@@ -41,7 +50,7 @@ fn main() {
         camera_pitch: 0.0,
         camera_yaw: 0.0,
         vfov: 1.0,
-        area: IVec2::new(0, 0)..IVec2::new(32, 16),
+        area,
     };
 
     let world = World::new(Path::new(&world_path), &options);
